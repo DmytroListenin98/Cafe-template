86cd07a6f19c937b5ab09155a2d00aeb258e8292
TrainLeft <dimalistenin070906@gmail.com>
2025-09-12 13:41:16 +0200

    Initial Commit

---
+++ coffee app/coffee_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_appApp.swift
+//  coffee app
+//
+//  Created by jabko on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct coffee_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
